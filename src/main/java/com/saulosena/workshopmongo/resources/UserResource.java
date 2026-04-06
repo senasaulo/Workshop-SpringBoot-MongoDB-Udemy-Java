de3cbaae2d9d8830dfbcd867e616b2cc4e9e910d
senasaulo <srs3@aluno.ifal.edu.br>
2026-04-06 14:49:20 -0300

    Inserção do padrão DTO

--- src/main/java/com/saulosena/workshopmongo/resources/UserResource.java
+++ src/main/java/com/saulosena/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.saulosena.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.saulosena.workshopmongo.domain.User;
+import com.saulosena.workshopmongo.dto.UserDTO;
 import com.saulosena.workshopmongo.services.UserService;
 
 @RestController
@@ -20,8 +22,9 @@ public class UserResource {
 	
 	// pode ser @RequestMapping(method = RequestMethod.GET)
 	@GetMapping 
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }

fb615055526f23114e4f58b76a45dd3ab0fde80e
Saulo Sena <srs3@aluno.ifal.edu.br>
2026-04-07 20:42:58 -0300

    Consulta com varios criterios

--- src/main/java/com/saulosena/workshopmongo/services/PostService.java
+++ src/main/java/com/saulosena/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.saulosena.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,11 @@ public class PostService {
 	public List<Post> findByTitle(String title){
 		//return repo.findByTitleContainingIgnoreCase(title);
 		return repo.searchTitle(title);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return repo.fullSearch( text,  minDate, maxDate );
 	}
 	
 }

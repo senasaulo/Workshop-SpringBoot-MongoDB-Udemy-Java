3f874b19a5ef5c06f685b0f73d7999c265f60a47
senasaulo <srs3@aluno.ifal.edu.br>
2026-04-07 12:08:57 -0300

    Criando entidade Post com User alinhado

---
+++ src/main/java/com/saulosena/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.saulosena.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.saulosena.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+	
+}

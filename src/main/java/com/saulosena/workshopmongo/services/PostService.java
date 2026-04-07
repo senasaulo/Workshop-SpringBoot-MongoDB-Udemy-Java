66e96f98f7d1f8dc60282e4c3029b8c86315d823
senasaulo <srs3@aluno.ifal.edu.br>
2026-04-07 15:15:26 -0300

    Retornando post por id do post e pelo id do usuario

---
+++ src/main/java/com/saulosena/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.saulosena.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.saulosena.workshopmongo.domain.Post;
+import com.saulosena.workshopmongo.repository.PostRepository;
+import com.saulosena.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+		public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	
+}
